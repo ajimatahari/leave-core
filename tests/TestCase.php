f416b4c4fa69d5f4a8a1c82259705f5dd0b5d666
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-10-15 12:16:45 +0800

    Added assertClassExist

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,4 +6,8 @@ use PHPUnit\Framework\TestCase as PHPUnit;
 
 class TestCase extends PHPUnit
 {
+    public function assertClassExist($class)
+    {
+        $this->assertTrue(class_exists($class));
+    }
 }
