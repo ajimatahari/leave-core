efcefdb72af151811198d9f09e028718442511ac
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-10-15 12:43:22 +0800

    Added Type Doc Block

--- src/Concerns/Type.php
+++ src/Concerns/Type.php
@@ -6,8 +6,18 @@ use CleaniqueCoders\Leave\Contracts\Type as TypeContract;
 
 trait Type
 {
+    /**
+     * Type of the Leave.
+     *
+     * @var string
+     */
     protected $type = '';
 
+    /**
+     * Set type of the leave.
+     *
+     * @param string $type
+     */
     public function setType(string $type): TypeContract
     {
         $this->type = $type;
@@ -15,6 +25,11 @@ trait Type
         return $this;
     }
 
+    /**
+     * Get type of the leave.
+     *
+     * @return string
+     */
     public function getType(): string
     {
         return $this->type;
